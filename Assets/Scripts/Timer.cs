c500689a61db8cb24150754165ca6259b68fd57a
jemayleow <jl1017815@student.francistuttle.edu>
2024-04-08 10:43:13 -0500

    Created functional timer script

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,18 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
+    public float timer;
+    private bool end;
+    //public static class Math;
+    public Text timerDisplay;
+    public bool End
+    {
+        get{return end;}
+    }
     // Start is called before the first frame update
     void Start()
     {
+        timer = 30;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                timerDisplay.text = ("Timer: 0");
+                end = true;
+            }
+            else
+            {
+                timerDisplay.text = ("Timer: "+Math.Round(timer));
+            }
+        }
     }
 }
