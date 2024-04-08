e3e02ce6a146b5c631e5864f2851db954923371e
aurialt <at1007641@student.francistuttle.edu>
2024-04-08 10:50:16 -0500

    Worked on car movement and the camera implementation
    Versions of cars located in AurialCarPrefabs

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
+    CharacterController characterController;
+    GameObject camera;
+
+    private Vector3 moveDirection;
+    
+    public float moveSpeed = 5.0f;
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        camera = gameObject.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
-    {
-        
+    {    
+        moveDirection.Normalize();
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void AddMoveInput(float forwardInput, float rightInput) {
+        Vector3 forward = camera.transform.forward;
+        Vector3 right = camera.transform.right;
+        moveDirection = (forwardInput * forward) + (rightInput * right);
+    }
+
+    public void changeCameraDirection(float rightInput) { 
+        gameObject.transform.Rotate(0,rightInput,0);
+        camera.transform.rotation.SetLookRotation(gameObject.transform.forward);
     }
 }
---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+    GameObject camera;
+    Car car;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera = gameObject.transform.GetChild(0).gameObject;
+        car = GetComponent<Car>();
+        print(camera);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMoveInput();
+    }
+
+    void HandleMoveInput() {
+        float forwardInput = Input.GetAxis("Vertical");
+        float rightInput = Input.GetAxis("Horizontal");
+        
+        car.AddMoveInput(forwardInput, rightInput);
+        car.changeCameraDirection(rightInput);
+        
+    }
+    
+}
